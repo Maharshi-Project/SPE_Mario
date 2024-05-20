094a1bb63d92159d5f3953787eba87d1f3bbdd86
Maharshi Patel <156591746+Maharshi-Project@users.noreply.github.com>
2024-05-20 12:32:09 +0530

    Update game.js

--- mario-frontend/src/game.js
+++ mario-frontend/src/game.js
@@ -261,7 +261,7 @@ kaboom({
     async function saveUser(username, score) {
     const data = { username, score };
     console.log('test function');
-    const response = await fetch('http://localhost:8081/saveUser', {
+    const response = await fetch('http://mario-backend:8081/saveUser', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
