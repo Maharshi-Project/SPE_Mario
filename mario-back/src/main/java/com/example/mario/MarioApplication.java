f01d0a5ec7f8de02fb2b8eaa3e05db32b33c283c
Maharshi <maharshipate20@gmail.com>
2024-05-21 16:21:08 +0530

    Updated fluentd file

--- mario-back/src/main/java/com/example/mario/MarioApplication.java
+++ mario-back/src/main/java/com/example/mario/MarioApplication.java
@@ -1,13 +1,19 @@
 package com.example.mario;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class MarioApplication {
 
+	private static final Logger logger = LogManager.getLogger(MarioApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MarioApplication.class, args);
+		logger.info("Start of Execution");
+		System.out.println("\nInside Main Class");
 	}
-
 }
